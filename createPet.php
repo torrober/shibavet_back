12ad1931891a676597eace147d23dfb3bce20b31
Roberto M. Rocha <robemario124@gmail.com>
2022-11-17 00:08:48 -0500

    create pet and get pet files done

--- createPet.php
+++ createPet.php
@@ -3,6 +3,19 @@ include('connection.php');
 include('headers.php');
 if (count($_POST) == 5) {
     //code for inserting pet
+    $query = "INSERT INTO pets (name, race, age, sex, ownerID) values ('{$_POST['name']}', '{$_POST['race']}', {$_POST['age']}, '{$_POST['sex']}', {$_POST['ownerID']})";
+    try {
+        if(!mysqli_query($con, $query)) {
+            $response = array("response" => "failed", "error" => mysqli_error($con), "content" => "");
+            echo json_encode($response, JSON_PRETTY_PRINT);
+        } else {
+            $response = array("response" => "ok", "error" => "null", "content" => "pet inserted");
+            echo json_encode($response, JSON_PRETTY_PRINT);
+        }
+    } catch(Exception $e) {
+        $response = array("response" => "failed", "error" => $e->getMessage(), "content" => "");
+        echo json_encode($response, JSON_PRETTY_PRINT);
+    }
 } else {
     $response = array("response" => "failed", "error" => "Missing data from request", "content" => []);
     echo json_encode($response, JSON_PRETTY_PRINT);
